5629dea5201cad1cbac8224cdf40919598c09200
CosbyK <mwathikuria@gmail.com>
2023-05-31 00:01:59 +0000

    1-square.py1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''A class representing Squares.'''
+
+
+class Square:
+    """Square representation."""
+   
+    def __init__(self, size):
+     """Load a new Square instance.
+
+        Args:
+            size (int): Size of the new square.
+        """
+     self.__size = size
