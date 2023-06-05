c450e6cea657d249dd1188383572464d3c81d226
CosbyK <mwathikuria@gmail.com>
2023-06-05 20:17:11 +0000

    #0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 '''Define the Rectangle class'''
+
+
 class Rectangle:
     def __init__(self):
-        '''Initialize a new instance of the Rectangle class.'''
+        '''Initialize a new instance
+        of the Rectangle class.'''
         pass
