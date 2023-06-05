9a36378bbb2f3a979251b128273d99f5ec44d52e
CosbyK <mwathikuria@gmail.com>
2023-06-05 21:09:51 +0000

    #2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+'''Define rectangle class'''
+
+
+class Rectangle:
+    '''Represent rectangle'''
+
+    def __init__(self, width=0, height=0):
+        '''Initializes rectangle
+
+        Args:
+            width (int): Width of rectangle
+            height (int): Height of rectangle
+        '''
+        self._Rectangle_width = width
+        self._Rectangle_height = height
+
+    @property
+    def width(self):
+        '''Get width of rectangle'''
+        return self._Rectangle_width
+
+    @width.setter
+    def width(self, value):
+        '''Set width of rectangle'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._Rectangle_width = value
+
+    @property
+    def height(self):
+        '''Get height of rectangle'''
+        return self._Rectangle_height
+
+    @height.setter
+    def height(self, value):
+        '''Set height of rectangle'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._Rectangle_height = value
+
+    def area(self):
+        '''Return area of rectangle'''
+        return self._Rectangle_width * self._Rectangle_height
+
+    def perimeter(self):
+        '''Return perimeter of rectangle'''
+        if self._Rectangle_width == 0 or self._Rectangle_height == 0:
+            return 0
+        return 2 * (self._Rectangle_width + self._Rectangle_height)
