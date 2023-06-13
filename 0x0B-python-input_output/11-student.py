a222454df5878cda471c6df74fea7d2aa82be436
CosbyK <mwathikuria@gmail.com>
2023-06-13 18:36:27 +0000

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+Defines class student
+'''
+
+
+class Student:
+    '''
+    Represents student
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+        Create new student
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        Represents student.
+        if attrs is a list of strings represents only attributes included in the list
+        '''
+        if attrs is None:
+            return self.__dict__
+        json_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_dict[attr] = getattr(self, attr)
+        return json_dict
